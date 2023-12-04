ee2493ed42cedec0fff1d167ef1aafa9b4d79a6b
EmirlanAbdullaev <134844359+EmirlanAbdullaev@users.noreply.github.com>
2023-12-04 04:04:22 +0300

    70% complete

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,24 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      Car car1 = new Car("TATRA-ЗиЛ 815",21);
+      Car car2 = new Car("Lexus LX",600);
+      Car car3 = new Car("ВАЗ (LADA) ",2106);
+      User user1 = new User("Emir1","Абдуллаев","emkaab@gmail.com");
+      User user2 = new User("Emir2","Абдуллаев","emkaab@gmail.com");
+      User user3 = new User("Emir3","Абдуллаев","emkaab@gmail.com");
+      user1.setCar(car1);
+      user2.setCar(car2);
+      user3.setCar(car3);
+
+      userService.add(user1);
+      userService.add(user2);
+      userService.add(user3);
+//      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+//      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+      System.out.println("-------------------");
+      System.out.println(userService.getByCar("TATRA-ЗиЛ 815",21));
+
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,6 +41,7 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = "+user.getCar());
          System.out.println();
       }
 
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -1,10 +1,14 @@
 package hiber.service;
 
+import hiber.model.Car;
 import hiber.model.User;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface UserService {
     void add(User user);
     List<User> listUsers();
+    public User getByCar (String model, int series);
 }
